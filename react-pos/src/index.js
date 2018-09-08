171a4e244aec6e722fcbba788a8338bac9929bb3
Prajwal Bati <prajwalbati@gmail.com>
2018-09-09 00:51:54 +0545

    files uploaded

--- react-pos/src/index.js
+++ react-pos/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+// import { makeRoutes } from "./routes";
+import App from "./App";
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
